3736d3386b06c6c1a5415a9a8c7c791d10ca0bdb
Chuma Okoro <chuma@nabsack.com>
2021-03-27 23:09:30 -0400

    clean up tree traversals

--- TreeTraversals.py
+++ TreeTraversals.py
@@ -1,90 +1,58 @@
-import queue
-from Graph import Graph
+from TreeNode import TreeNode
 
-# input - list of lists - adjacency matrix
-def bfs_on_am_graph(graph: list, start: int):
-    print("Beginning Breadth first search on matrix graph...")
-    if len(graph) is 0:
-        return
-    visited = set()
-    to_visit_queue = queue.Queue()
-    to_visit_queue.put(start)
 
-    while not to_visit_queue.empty():
-        # we take the next node off the queue to explore it's neighbors (1 node away)
-        node = to_visit_queue.get()
+def createMockTree() -> TreeNode:
+    node_1 = TreeNode('a')
+    node_2 = TreeNode('b')
+    node_3 = TreeNode('c')
+    node_4 = TreeNode('d')
+    node_5 = TreeNode('e')
+    node_6 = TreeNode('f')
+    node_7 = TreeNode('g')
+    node_8 = TreeNode('h')
+    node_9 = TreeNode('i')
 
-        if node not in visited:
-            print("Explore node on matrix graph w/ BFS : ", node)
+    node_1.left = node_2
+    node_1.right = node_3
 
-            for neighbor in range(len(graph[node])):
-                # if we have an edge and haven't visited yet
-                if graph[node][neighbor] == 1 and neighbor not in visited:
-                    to_visit_queue.put(neighbor)
-            visited.add(node)
+    node_2.left = node_4
+    node_2.right = node_5
+
+    node_3.left = node_6
+    node_3.right = node_7
+
+    node_5.left = node_8
+    node_7.right = node_9
+
+    #            a
+    #     b             c
+    # d       e     f       g
+    #       h                  i
+
+    return node_1
+
+
+def printTreeInOrder(root: TreeNode):
+    # there is in order, pre order, post order
+    # you just need to change the order of this code!
+    # no cycles in trees
+    if root.left is not None:
+        printTreeInOrder(root.left)
+    print("In order: ", root.value)
+
+    if root.right is not None:
+        printTreeInOrder(root.right)
+
+
+def bfs_on_tree(root: TreeNode):
+    node_queue = [root]
+
+    while len(node_queue) != 0:
+        node = node_queue.pop(0)
+        print("Node: ", node.value)
+
+        if node.left is not None:
+            node_queue.append(node.left)
+        if node.right is not None:
+            node_queue.append(node.right)
     return
-
-
-# input - list of lists - adjacency matrix
-def dfs_on_am_graph(graph: list, start: int, visited=None):
-    # failsafe checks, this shouldn't happen tho
-    if len(graph) == 0 or start > len(graph) - 1:
-        return
-
-    if visited is None:
-        print("Beginning Depth first search on matrix graph...")
-        visited = set()
-
-    visited.add(start)
-    print("Explore node on matrix graph w/ DFS : ", start)
-
-    for neighbor in range(len(graph[start])):
-        if neighbor not in visited and graph[start][neighbor] == 1:
-            dfs_on_am_graph(graph, neighbor, visited)
-
-
-# input - graph object
-def bfs_on_object_graph(graph: Graph, start: str):
-    # if len(graph) is 0:
-    #     return
-    print("Beginning Breadth first search on object graph...")
-    visited = set()
-    to_visit_queue = queue.Queue()
-    to_visit_queue.put(start)
-
-    while not to_visit_queue.empty():
-        # we take the next node off the queue to explore it's neighbors (1 node away)
-        node = to_visit_queue.get()
-
-        if node not in visited:
-            print("Explore node on object graph w/ BFS : ", node)
-
-            for neighbor in graph.getConnectedNodes(node):
-                # if we have an edge and haven't visited yet
-                if neighbor not in visited:
-                    to_visit_queue.put(neighbor)
-            visited.add(node)
-    return
-
-
-# input - graph object
-def dfs_on_object_graph(graph: Graph, start: str, visited=None):
-    nodes = graph.getVertices()
-    if len(nodes) == 0 or start not in nodes:
-        return
-
-    if visited is None:
-        # should be first time being called
-        print("Beginning Depth first search on object graph...")
-        visited = set()
-
-    if start not in visited:
-        print("Explore node on object graph w/ DFS : ", start)
-
-        visited.add(start)
-        neighbors = graph.getConnectedNodes(start)
-
-        for neighbor in neighbors:
-            # we want to search the depth of every node we have not visited yet
-            if neighbor not in visited:
-                dfs_on_object_graph(graph, neighbor, visited)
