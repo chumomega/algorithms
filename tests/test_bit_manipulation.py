67be00d4036b060520fadc757085b4493690e442
Chuma Okoro <chuma.okoro76@gmail.com>
2025-02-02 10:37:08 -0500

    add more unit test

--- tests/test_bit_manipulation.py
+++ tests/test_bit_manipulation.py
@@ -7,6 +7,30 @@ def test_add_wo_sum():
     expected_sum = 320
     actual_sum = add_wo_plus(num1, num2)
 
+    assert 320 == actual_sum
+
+    num1 = -241
+    num2 = 79
+
+    expected_sum = -162
+    actual_sum = add_wo_plus(num1, num2)
+
+    assert expected_sum == actual_sum
+
+    num1 = 0
+    num2 = 79
+
+    expected_sum = 79
+    actual_sum = add_wo_plus(num1, num2)
+
+    assert expected_sum == actual_sum
+
+    num1 = -13
+    num2 = 0
+
+    expected_sum = -13
+    actual_sum = add_wo_plus(num1, num2)
+
     assert expected_sum == actual_sum
     
 
